96a01a8c2b61058fe7e4f30c7d7245523b203b3d
Chris Ware <c.p.ware@hotmail.com>
2023-11-09 11:56:34 -0700

    Nav Start

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,16 +7,14 @@ const Hero = () => {
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.left}>
-            <h1>Main Heading</h1>
-            <p>Main paragraph here</p>
-            <div className={styles.btnCointainer}>
-              2 btns here
-            </div>
+          <h1>Modern rental luxury complex</h1>
+          <p>Main paragraph here</p>
+          <div className={styles.btnCointainer}>2 btns here</div>
         </div>
         <div className={styles.right}>
           background Image here. pos rel.
           <div className={styles.r1}>
-            Rotating circle btn here leading to the next section of the site. 
+            Rotating circle btn here leading to the next section of the site.
           </div>
         </div>
       </div>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,6 +1,67 @@
 import styles from "./Nav.module.css";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const openMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (body) {
+      if (window.innerWidth <= 768 && isOpen) {
+        body.style.overflow = "hidden";
+      } else {
+        body.style.overflow = "auto";
+      }
+    }
+
+    const handleResize = () => {
+      setIsOpen(false);
+      window.addEventListener("resize", handleResize);
+    };
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (body) {
+        body.style.overflow = "auto";
+      }
+    };
+  }, [isOpen]);
+
+  const pathname = usePathname();
+
+  // const navItems = [
+  //   {
+  //     text: "Home",
+  //     href: "/",
+  //   },
+  //   {
+  //     text: "services",
+  //     href: "/services",
+  //   },
+  //   {
+  //     text: "Portfolio",
+  //     href: "/portfolio",
+  //   },
+  //   {
+  //     text: "Pricing",
+  //     href: "/pricing",
+  //   },
+  //   {
+  //     text: "About",
+  //     href: "/about",
+  //   },
+  //   {
+  //     text: "Contact",
+  //     href: "/contact",
+  //   },
+  // ];
+
   const currentDate = new Date();
 
   const monthNames = [
@@ -24,7 +85,9 @@ function Nav() {
 
   return (
     <header className={styles.container}>
-      <div className={styles.preNav}>{message}</div>
+      <div className={styles.preNav}>
+        {message} <span className={styles.span}>Contact us</span>
+      </div>
       <nav className={styles.content}>Nav Here</nav>
     </header>
   );
