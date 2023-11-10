64ae6ac94586e52d32269d276424ecb4d86052fa
Chris Ware <c.p.ware@hotmail.com>
2023-11-09 23:47:51 -0700

    Challenge + Solution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero/Hero";
+import Challenge from "@/components/PageSections/Challenge/Challenge";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <Challenge />
     </div>
   );
 };
---
+++ src/components/PageSections/Challenge/Challenge.tsx
@@ -0,0 +1,10 @@
+import styles from "./Challenge.module.css";
+
+const Challenge = () => {
+  return (
+    <div>
+      <h2>Challenge Sollution here</h2>
+    </div>
+  );
+};
+export default Challenge;
