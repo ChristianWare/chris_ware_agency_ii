7a925a5ea49a77a1953e24b4e45fe0966a45d71f
Chris Ware <c.p.ware@hotmail.com>
2023-11-10 18:04:43 -0700

    Benefit Desktop Done

--- src/components/PageSections/Benefits/Benefits.tsx
+++ src/components/PageSections/Benefits/Benefits.tsx
@@ -1,46 +1,67 @@
+import Image from "next/image";
 import LayoutWrapper from "@/components/Layout/LayoutWrapper";
 import styles from "./Benefits.module.css";
 import ContentPadding from "@/components/Layout/ContentPadding/ContentPadding";
 import { benefits } from "@/lib/data";
+import Check from "../../../../public/icons/check.svg";
+import Img1 from "../../../../public/images/img2.png";
 
 const Benefits = () => {
   return (
-    <div className={styles.content}>
-      <LayoutWrapper>
-        <ContentPadding>
+    <LayoutWrapper>
+      <ContentPadding>
+        <div className={styles.content}>
           <div className={styles.top}>
+            <div className={styles.b2}>
+              <div className={styles.imgContainer}>
+                <Image
+                  src={Img1}
+                  alt='image'
+                  fill
+                  objectFit='cover'
+                  className={styles.img}
+                />
+              </div>
+            </div>
             <h2 className={styles.heading}>
-              The benefits of a direct booking website
+              Benefits of a direct booking website
             </h2>
+
             <p className={styles.headingCopy}>
               Having a direct booking website for your vacation rental property
-              instead of relying solely on Airbnb and VRBO (Vacation Rental By
-              Owner) can offer several benefits. Here are some of the advantages
-              of having your own booking website: - 18px
+              instead of relying solely on Airbnb and VRBO can offer several
+              benefits. Here are some of the advantages of having your own
+              booking website:
             </p>
           </div>
           <div className={styles.bottom}>
             <div className={styles.b1}>
               {benefits.slice(0, 2).map((benefit, index) => (
                 <div key={index} className={styles.benefitContainer}>
-                  <h3 className={styles.benefitHeading}>{benefit.heading}</h3>
+                  <span className={styles.span}>
+                    <Check width={20} height={20} className={styles.icon} />
+                    <h3 className={styles.benefitHeading}>{benefit.heading}</h3>
+                  </span>
                   <p className={styles.benefitDesc}>{benefit.description}</p>
                 </div>
               ))}
             </div>
-            <div className={styles.b2}>circle</div>
+
             <div className={styles.b3}>
               {benefits.slice(2, 4).map((benefit, index) => (
                 <div key={index} className={styles.benefitContainer}>
-                  <h3 className={styles.benefitHeading}>{benefit.heading}</h3>
+                  <span className={styles.span}>
+                    <Check width={20} height={20} className={styles.icon} />
+                    <h3 className={styles.benefitHeading}>{benefit.heading}</h3>
+                  </span>
                   <p className={styles.benefitDesc}>{benefit.description}</p>
                 </div>
               ))}
             </div>
           </div>
-        </ContentPadding>
-      </LayoutWrapper>
-    </div>
+        </div>
+      </ContentPadding>
+    </LayoutWrapper>
   );
 };
 
