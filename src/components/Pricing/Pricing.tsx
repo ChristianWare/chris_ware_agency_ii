441de4b53154bc3db768b0edd8c0da35e6fc2567
Chris Ware <c.p.ware@hotmail.com>
2023-11-14 02:17:20 -0700

    Pricing update

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -23,22 +23,24 @@ const Pricing = () => {
         <div className={styles.bottom}>
           {pricing.map((x) => (
             <div key={x.id} className={styles.card}>
-              <h3 className={styles.planName}>{x.plan}</h3>
-              <p className={styles.description}>{x.description}</p>
-              <hr className={styles.hr} />
-              <strong className={styles.price}>
-                $ {x.price} <span className={styles.span}>/ mo</span>
-              </strong>
-              <p className={styles.includes}>Includes:</p>
-              <div className={styles.checkFeatureContainer}>
-                {x.includes.map((y, index) => (
-                  <ul key={index} className={styles.featuresList}>
-                    <li>
-                      <Check width={30} height={30} className={styles.icon} />{" "}
-                      {y.feature}
-                    </li>
-                  </ul>
-                ))}
+              <div className={styles.box}>
+                <h3 className={styles.planName}>{x.plan}</h3>
+                <p className={styles.description}>{x.description}</p>
+                <hr className={styles.hr} />
+                <strong className={styles.price}>
+                  $ {x.price} <span className={styles.span}>/ mo</span>
+                </strong>
+                <p className={styles.includes}>Includes:</p>
+                <div className={styles.checkFeatureContainer}>
+                  {x.includes.map((y, index) => (
+                    <ul key={index} className={styles.featuresList}>
+                      <li>
+                        <Check width={30} height={30} className={styles.icon} />{" "}
+                        {y.feature}
+                      </li>
+                    </ul>
+                  ))}
+                </div>
               </div>
             </div>
           ))}
