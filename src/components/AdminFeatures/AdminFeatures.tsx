905081aab36bc6c504ca34756429a805599b6a5c
Chris Ware <c.p.ware@hotmail.com>
2023-11-17 01:33:08 -0700

    Admin + User done

--- src/components/AdminFeatures/AdminFeatures.tsx
+++ src/components/AdminFeatures/AdminFeatures.tsx
@@ -1,12 +1,41 @@
 import ContentPadding from "../Layout/ContentPadding/ContentPadding";
 import LayoutWrapper from "../Layout/LayoutWrapper";
 import styles from "./AdminFeatures.module.css";
+import Image from "next/image";
+import Img from "../../../public/images/img1.png";
+import { adminData } from "@/lib/data";
 
 const AdminFeatures = () => {
   return (
     <LayoutWrapper>
       <ContentPadding>
-        <div >AdminFeatures</div>
+        <div className={styles.content}>
+          <h2 className={styles.heading}>
+            <span className={styles.highlight}>Administrative</span> access
+            includes:
+          </h2>
+          <div className={styles.mapBox}>
+            {adminData.map((x, index) => (
+              <div key={index} className={styles.cardContainer}>
+                <div className={styles.card}>
+                  <div className={styles.left}>
+                    <h3 className={styles.category}>{x.category}</h3>
+                    {x.tasks.map((y, i) => (
+                      <p key={i} className={styles.task}>
+                        • {y.task}
+                      </p>
+                    ))}
+                  </div>
+                  <div className={styles.right}>
+                    <div className={styles.imgContainer}>
+                      <Image src={Img} alt='logo' fill className={styles.img} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </ContentPadding>
     </LayoutWrapper>
   );
--- src/components/UserFeatures/UserFeatures.tsx
+++ src/components/UserFeatures/UserFeatures.tsx
@@ -1,12 +1,42 @@
+import Image from "next/image";
 import ContentPadding from "../Layout/ContentPadding/ContentPadding";
 import LayoutWrapper from "../Layout/LayoutWrapper";
 import styles from "./UserFeatures.module.css";
+import { guestData } from "@/lib/data";
+import Img from "../../../public/images/img1.png";
+
 
 const UserFeatures = () => {
   return (
     <LayoutWrapper>
       <ContentPadding>
-        <div>UserFeatures</div>
+        <div className={styles.content}>
+          <h2 className={styles.heading}>
+            <span className={styles.highlight}>Guest</span> access
+            includes:
+          </h2>
+          <div className={styles.mapBox}>
+            {guestData.map((x, index) => (
+              <div key={index} className={styles.cardContainer}>
+                <div className={styles.card}>
+                  <div className={styles.left}>
+                    <h3 className={styles.category}>{x.category}</h3>
+                    {x.tasks.map((y, i) => (
+                      <p key={i} className={styles.task}>
+                        • {y.task}
+                      </p>
+                    ))}
+                  </div>
+                  <div className={styles.right}>
+                    <div className={styles.imgContainer}>
+                      <Image src={Img} alt='logo' fill className={styles.img} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </ContentPadding>
     </LayoutWrapper>
   );
