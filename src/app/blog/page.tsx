596a071c89b98874bbb851bbfc3e2e8bb94abdc7
Chris Ware <c.p.ware@hotmail.com>
2023-11-26 15:52:56 -0700

    Pre hero fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -27,7 +27,7 @@ const BlogPage = () => {
     <>
       <PageIntro
         labelText='Blog'
-        heading='Our Insights'
+        heading='The Chris Ware Blog'
         copy="We're on a mission to redefine the vacation rental experience by empowering property owners to seize control of their bookings. Discover the narrative behind our commitment to personalized, direct booking websites and join us in transforming the way you connect with your guests."
         src={Img}
       />
--- src/components/PreHero/PreHero.tsx
+++ src/components/PreHero/PreHero.tsx
@@ -3,32 +3,65 @@ import ContentPadding from "../Layout/ContentPadding/ContentPadding";
 import LayoutWrapper from "../Layout/LayoutWrapper";
 import styles from "./PreHero.module.css";
 import Image from "next/image";
+import Label from "../Label/Label";
 
 const PreHero = () => {
   return (
-    <div className={styles.bg}>
-      <div className={styles.layoutWrapper}>
-        <div className={styles.contentPadding}>
-          <div className={styles.content}>
-            {preHero.map((x, index) => (
-              <div key={index} className={styles.container}>
-                <div className={styles.iconContainer}>
-                  <Image
-                    src={x.icon}
-                    alt='icon'
-                    width={40}
-                    height={40}
-                    className={styles.icon}
-                  />
-                </div>
-                <h2 className={styles.feature}>{x.feature}</h2>
-                <p className={styles.desc}>{x.desc}</p>
-              </div>
-            ))}
+    // <div className={styles.bg}>
+    //   <div className={styles.layoutWrapper}>
+    //     <div className={styles.contentPadding}>
+    //       <div className={styles.content}>
+    //         {preHero.map((x, index) => (
+    //           <div key={index} className={styles.container}>
+    //             <div className={styles.iconContainer}>
+    //               <Image
+    //                 src={x.icon}
+    //                 alt='icon'
+    //                 width={40}
+    //                 height={40}
+    //                 className={styles.icon}
+    //               />
+    //             </div>
+    //             <h2 className={styles.feature}>{x.feature}</h2>
+    //             <p className={styles.desc}>{x.desc}</p>
+    //           </div>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <LayoutWrapper>
+      <ContentPadding>
+        <div className={styles.contentTop}>
+          <div className={styles.left}>
+            <Label text='core principles and beliefs' />
+            <h2 className={styles.heading}>Our Values</h2>
+          </div>
+          <div className={styles.right}>
+            <p className={styles.topCopy}>
+              Our values act as a set of guiding principles and fundamental
+              beliefs that influence the behaviors we demonstrate, which shape
+              the decisions we collectively make as a team.
+            </p>
           </div>
         </div>
-      </div>
-    </div>
+        <div className={styles.contentBottom}>
+          {preHero.map((x, index) => (
+            <div key={index} className={styles.box}>
+              <Image
+                src={x.icon}
+                alt='icon'
+                width={40}
+                height={40}
+                className={styles.icon}
+              />
+              <h3 className={styles.value}>{x.feature}</h3>
+              <p className={styles.description}>{x.desc}</p>
+            </div>
+          ))}
+        </div>
+      </ContentPadding>
+    </LayoutWrapper>
   );
 };
 export default PreHero;
