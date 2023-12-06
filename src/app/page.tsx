c6554da42d60c053e1bcdca60d282ba5488d98dd
Chris Ware <c.p.ware@hotmail.com>
2023-12-06 11:45:15 -0700

    Added portfolio section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ const HomePage = () => {
       <Services />
       <HowItWorks />
       <Pricing />
-      {/* <Portfolio /> */}
+      <Portfolio />
       <Faq />
       <BlogSection blogData={blogs} />
       <FinalCta />
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import ContentPadding from "../Layout/ContentPadding/ContentPadding";
 import LayoutWrapper from "../Layout/LayoutWrapper";
 import SectionHeading from "../SectionHeading/SectionHeading";
@@ -7,6 +9,8 @@ import Img1 from "../../../public/images/img2.png";
 import Button from "../Button/Button";
 import { caseStudies } from "@/lib/data";
 import Label from "../Label/Label";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../animation/variants";
 
 const Portfolio = () => {
   return (
@@ -16,7 +20,14 @@ const Portfolio = () => {
           <SectionHeading headingText='Case Studies' />
         </div>
         {caseStudies.map((x, index) => (
-          <div className={styles.bottom} key={index}>
+          <motion.div
+            variants={fadeIn("up", 0.3)}
+            initial='hidden'
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className={styles.bottom}
+            key={index}
+          >
             <div className={styles.left}>
               <div className={styles.imgContainer}>
                 <Image src={Img1} alt='hello' fill className={styles.img} />
@@ -24,15 +35,19 @@ const Portfolio = () => {
             </div>
             <div className={styles.right}>
               <div className={styles.container}>
-                <Label text={`Project ${index + 1}`} />
-                <h3 className={styles.company}>{x.company}</h3>
-                <p className={styles.projectDetails}>{x.projectDetails}</p>
-                <div className={styles.btnContainer}>
-                  <Button btnType='tertiaryii' href={x.href} text='Live site' />
+                <div className={styles.c1}>
+                  <Label text={`Project ${index + 1}`} />
+                  <h3 className={styles.company}>{x.company}</h3>
+                  <p className={styles.projectDetails}>{x.projectDetails}</p>
+                </div>
+                <div className={styles.c2}>
+                  <div className={styles.btnContainer}>
+                    <Button btnType='tertiary' href={x.href} text='Live site' />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </ContentPadding>
     </LayoutWrapper>
