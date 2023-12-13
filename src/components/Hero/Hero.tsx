a67db68abaf74f8af69b2c4c9b6dbbb908b37295
Chris Ware <c.p.ware@hotmail.com>
2023-12-12 21:37:00 -0700

    Nav update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -16,7 +16,14 @@ const Hero = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imgOverlay}></div>
-      <Image src={Img} alt='img' fill className={styles.img} />
+      <Image
+        src={Img}
+        alt='img'
+        fill
+        className={styles.img}
+        sizes='(max-width: 768px) 50vw, (max-width: 1200px) 100vw'
+        priority
+      />
       <Nav />
       <LayoutWrapper>
         <ContentPadding>
